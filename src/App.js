f735a9e38c32b0a69e8da98eb80ba30942f2c7f5
AyoOluwa-Israel <izzyfresher@gmail.com>
2021-09-15 08:59:09 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
 import './App.css';
+import Cards from './Cards';
+
+const url = 'https://api.enye.tech/v1/challenge/records'
 
 function App() {
+
+  const [loading, setLoading] = useState(false)
+  const [records, setRecords] = useState([])
+
+  const fetchRecords = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      
+      console.log(data.records)
+      const profiles  = data.records;
+      
+      // if(records){
+      //   const newRecords = data.records.map((item) => {
+      //     const {FirstName, LastName, Gender} = item;
+      //     return{
+      //       firstname: FirstName,
+      //       lastname: LastName,
+      //       gender: Gender
+      //     }
+        setRecords(profiles)
+      // setRecords(newRecords)
+      // console.log(newRecords);
+      // }else{
+      //   setRecords([])
+      // }
+      console.log(profiles);
+      
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    fetchRecords()
+  }, [])
+
+  // if (loading) {
+  //   return (
+  //     <main>
+  //       <Loading />
+  //     </main>
+  //   )
+  // }
+// console.log(records.records.profiles);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        Hello world
+        <h1>
+          
+        </h1>
+      <Cards />
     </div>
   );
 }
